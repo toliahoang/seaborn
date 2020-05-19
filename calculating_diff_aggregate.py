4b0804ea41cf93e432e2188b986f60edf52d3420
toliahoang <tung.istu@gmail.com>
2020-05-19 15:33:58 +0700

    Create calculating_diff_aggregate.py

---
+++ calculating_diff_aggregate.py
@@ -0,0 +1,12 @@
+import codecademylib3_seaborn
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("survey.csv")
+print(df)
+sns.barplot(data=df,x='Gender',y='Response',estimator=np.median)
+plt.show()
