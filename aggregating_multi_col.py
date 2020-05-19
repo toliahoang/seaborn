c382bb80f2101cde1ff3de4142122b144307abad
toliahoang <tung.istu@gmail.com>
2020-05-19 15:34:30 +0700

    Create aggregating_multi_col.py

---
+++ aggregating_multi_col.py
@@ -0,0 +1,9 @@
+import codecademylib3_seaborn
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("survey.csv")
+print(df)
+sns.barplot(data=df,x='Age Range',y='Response',hue='Gender')
+plt.show()
