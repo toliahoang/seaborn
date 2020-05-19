27654a86a4ab5c4e69e171d9c2a531f83dd4c748
toliahoang <tung.istu@gmail.com>
2020-05-19 15:27:39 +0700

    Create plotting_aggregate.py

---
+++ plotting_aggregate.py
@@ -0,0 +1,10 @@
+import codecademylib3_seaborn
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+gradebook = pd.read_csv("gradebook.csv")
+
+print(gradebook)
+sns.barplot(data=gradebook,x="assignment_name",y='grade')
+plt.show()
